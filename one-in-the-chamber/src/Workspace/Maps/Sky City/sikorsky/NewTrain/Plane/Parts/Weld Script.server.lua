a60387d7b0d3f613899478b0af4e618859f4d106
Harry Kim <harryk9395@gmail.com>
2025-08-07 18:00:57 -0700

    Add first place auras

--- one-in-the-chamber/src/Workspace/Maps/Sky City/sikorsky/NewTrain/Plane/Parts/Weld Script.server.lua
+++
@@ -1,9 +0,0 @@
---3000cLOnEs
---All the bricks must have a diffront name. If you need more the copy from local to the green tipeing.
-
-local w1 = Instance.new("Weld")
-
-w1.Parent = script.Parent.Engine
-w1.Part0 = w1.Parent
-w1.Part1 = script.Parent.Seat --Chaing this to the name of the brick
-w1.C1 = CFrame.fromEulerAnglesXYZ(0.3, 0, 0) * CFrame.new(0, 0, 0) --Chaing this to put the brick in a diffront place
